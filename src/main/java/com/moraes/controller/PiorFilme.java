dc3c4a32bb591a0b8fd9e09a5337d8633984f1d0
Martin Morães <martin.moraes@outlook.com>
2021-11-15 21:23:03 -0300

    refact: implementes application/json

--- src/main/java/com/moraes/controller/PiorFilme.java
+++ src/main/java/com/moraes/controller/PiorFilme.java
@@ -2,23 +2,23 @@ package com.moraes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.moraes.service.WorstProducer;
 import com.moraes.usecase.WorstProducerUseCase;
 
 
 @RestController
-@RequestMapping("/piorfilme")
+//@RequestMapping("/piorfilme")
 public class PiorFilme {
 
 	@Autowired
 	private WorstProducerUseCase worstProducerUseCase;
 	
 	
-	@GetMapping	
+//	@GetMapping	
+	@RequestMapping(value = "/piorfilme", method = RequestMethod.GET, produces="application/json")
 	public ResponseEntity<String> obterPior() {
 		String resultJson = worstProducerUseCase.execute();
 		return ResponseEntity.ok(resultJson);
